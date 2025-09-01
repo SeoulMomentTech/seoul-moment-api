9a6e7a0662ef715754c9dda5f468f185fbc8ad4d
Junnnnnnnnnnn <wjsguswls71@naver.com>
2025-09-01 17:50:05 +0900

    fix: lint

--- libs/repository/src/repository.module.ts
+++ libs/repository/src/repository.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BrandBannerImageEntity } from './entity/brand-banner-image.entity';
-import { BrandSectionEntity } from './entity/brand-section.entity';
 import { BrandSectionImageEntity } from './entity/brand-section-image.entity';
+import { BrandSectionEntity } from './entity/brand-section.entity';
 import { BrandEntity } from './entity/brand.entity';
 import { LanguageEntity } from './entity/language.entity';
 import { MultilingualTextEntity } from './entity/multilingual-text.entity';
