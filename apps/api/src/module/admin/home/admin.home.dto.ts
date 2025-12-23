06e7ab8e6dc7fa0a7d64b0109b8842bd8f341031
Junnnnnnnnnnn <wjsguswls71@naver.com>
2025-12-23 09:46:06 +0900

    feat: get admin/home/banner response data 추가

--- apps/api/src/module/admin/home/admin.home.dto.ts
+++ apps/api/src/module/admin/home/admin.home.dto.ts
@@ -1,7 +1,8 @@
 import { HomeBannerImageEntity } from '@app/repository/entity/home-banner-image.entity';
+import { HomeBannerStatus } from '@app/repository/enum/home-banner-image.enum';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
-import { IsInt, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class GetHomeBannerResponse {
   @ApiProperty({ description: '배너 이미지 ID', example: 1 })
@@ -22,11 +23,33 @@ export class GetHomeBannerResponse {
   @IsString()
   mobileImage: string;
 
+  @ApiProperty({
+    description: '배너 상태',
+    example: HomeBannerStatus.NORMAL,
+  })
+  @IsEnum(HomeBannerStatus)
+  status: HomeBannerStatus;
+
+  @ApiProperty({
+    description: '생성일',
+    example: '2025-01-01T12:00:00.000Z',
+  })
+  createDate: Date;
+
+  @ApiProperty({
+    description: '수정일',
+    example: '2025-01-01T12:00:00.000Z',
+  })
+  updateDate: Date;
+
   static from(entity: HomeBannerImageEntity) {
     return plainToInstance(this, {
       id: entity.id,
       image: entity.getImage(),
       mobileImage: entity.getMobileImage(),
+      status: entity.status,
+      createDate: entity.createDate,
+      updateDate: entity.updateDate,
     });
   }
 }
