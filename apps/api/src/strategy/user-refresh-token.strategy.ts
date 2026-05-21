22280504f25a03b993b309da863fd38e7e5d5d17
Junnnnnnnnnnn <wjsguswls71@naver.com>
2026-05-21 17:03:48 +0900

    refactor: remove expired token handling from UserRefreshTokenStrategy
    - Eliminated the expired token check in the UserRefreshTokenStrategy, simplifying the token validation logic.
    - This change focuses on streamlining the authentication process by relying on existing error handling mechanisms for expired tokens.
    
    These modifications enhance the clarity and maintainability of the user refresh token strategy.

--- apps/api/src/strategy/user-refresh-token.strategy.ts
+++ apps/api/src/strategy/user-refresh-token.strategy.ts
@@ -1,6 +1,4 @@
 import { JwtType } from '@app/auth/auth.dto';
-import { ServiceErrorCode } from '@app/common/exception/dto/exception.dto';
-import { ServiceError } from '@app/common/exception/service.error';
 import { Configuration } from '@app/config/configuration';
 import { UserRepositoryService } from '@app/repository/service/user.repository.service';
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
@@ -28,11 +26,6 @@ export class UserRefreshTokenStrategy extends PassportStrategy(
       throw new HttpException('Invalid token type', HttpStatus.FORBIDDEN);
 
     const token = ExtractJwt.fromAuthHeaderAsBearerToken()(request);
-
-    if (Date.now() > payload.exp * 1000) {
-      // ServiceError를 던져서 만료된 토큰을 처리
-      throw new ServiceError('please login', ServiceErrorCode.FORBIDDEN); // 만료된 토큰을 401로 처리
-    }
 
     const userEntity =
       await this.userRepositoryService.getUserByIdWithRefreshToken(payload.id);
